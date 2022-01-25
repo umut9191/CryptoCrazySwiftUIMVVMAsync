f0f4fbc8a08b9becb8fb6c924e03a7a7381be796
umut9191 <umutsurmeli91@gmail.com>
2022-01-25 22:22:49 +0300

    Initial Commit

---
+++ CryptoCrazySwiftUIMVVMAsync/CryptoCrazySwiftUIMVVMAsyncApp.swift
@@ -0,0 +1,17 @@
+//
+//  CryptoCrazySwiftUIMVVMAsyncApp.swift
+//  CryptoCrazySwiftUIMVVMAsync
+//
+//  Created by Mac on 25.01.2022.
+//
+
+import SwiftUI
+
+@main
+struct CryptoCrazySwiftUIMVVMAsyncApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
